3446b3f6b434dc5a16f77fb24104c33318a8ef20
Rawand Dargali <rdargali@yahoo.com>
2020-02-13 12:44:03 -0600

    modify header

---
+++ trip-planner-client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Trip Planner</h1>
+      <ul className="menuBar">
+        <li>Home</li>
+        <li>View Trips</li>
+        <li>Add Trip</li>
+      </ul>
+    </div>
+  );
+}
